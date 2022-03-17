c68e6d877d87988331bf391ed16780383a58eefa
Akshay Mankar <itsakshaymankar@gmail.com>
2022-03-17 13:03:40 +0530

    Implement update by query

--- src/Database/Bloodhound/Client.hs
+++ src/Database/Bloodhound/Client.hs
@@ -50,6 +50,7 @@ module Database.Bloodhound.Client
        -- ** Documents
        , indexDocument
        , updateDocument
+       , updateByQuery
        , getDocument
        , documentExists
        , deleteDocument
@@ -884,6 +885,16 @@ updateDocument (IndexName indexName)
   where url = addQuery (versionCtlParams cfg) <$>
               joinPath [indexName, mappingName, docId, "_update"]
         body = Just (encode $ object ["doc" .= toJSON patch])
+
+updateByQuery :: MonadBH m => IndexName -> Query -> Maybe Script -> m Reply
+updateByQuery (IndexName indexName) q mScript =
+  bindM2 post url (return body)
+  where url = joinPath [indexName, "_update_by_query"]
+        body = Just . encode $ ("query" .= q) <> scriptObject
+        scriptObject = case toJSON mScript of
+          Null -> mempty
+          Object o -> o
+          json -> "script"  .= json
 
 -- | 'deleteDocument' is the primary way to delete a single document.
 --
